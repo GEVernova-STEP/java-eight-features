b5339302a2d4d9027f4496894b3a7ab8ff49791c
ashutosh-3474 <ashutosh.2201062cs@iiitbh.ac.in>
2026-01-17 16:40:34 +0530

    Problem 7: Method Reference in stream operation

---
+++ src/main/java/com/gevernova/javaeightfeatures/methodreference/methodreference_streamoperation/Main.java
@@ -0,0 +1,23 @@
+package com.gevernova.javaeightfeatures.methodreference.methodreference_streamoperation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<String> list = new ArrayList<>();
+
+        System.out.print("Enter number of words: ");
+        int n = sc.nextInt();
+        for (int i=0;i<n;i++)
+        {
+            System.out.print("Enter Word: ");
+            list.add(sc.next());
+        }
+
+        list.stream().map(String::toUpperCase).forEach(System.out::println);
+
+    }
+}
