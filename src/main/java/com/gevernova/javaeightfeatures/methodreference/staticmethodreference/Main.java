cbd533fe98ddba1b7ef2607e264d975b2b3428b7
ashutosh-3474 <ashutosh.2201062cs@iiitbh.ac.in>
2026-01-17 13:40:47 +0530

    Problem 1: Static Method Reference

---
+++ src/main/java/com/gevernova/javaeightfeatures/methodreference/staticmethodreference/Main.java
@@ -0,0 +1,23 @@
+package com.gevernova.javaeightfeatures.methodreference.staticmethodreference;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static Integer square(int n)
+    {
+        return  n * n;
+    }
+    public static void main(String[] args) {
+
+
+        List<Integer> num = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6));
+
+        List<Integer> sqnum = num.stream().map(Main::square).toList();
+
+        System.out.println(sqnum);
+
+
+    }
+}
